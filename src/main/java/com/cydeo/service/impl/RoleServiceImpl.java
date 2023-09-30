a03e7d6b056bb3d8403a232ae5af1a23da2012a7
irfan <23pointalpha@gmail.com>
2023-09-30 21:42:52 +0300

    RoleServiceImpl updated, /user/create page Choose a Role dropdown filled with roles.

--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -1,19 +1,24 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.RoleDTO;
+import com.cydeo.entity.Role;
+import com.cydeo.mapper.RoleMapper;
 import com.cydeo.repository.RoleRepository;
 import com.cydeo.service.RoleService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RoleServiceImpl implements RoleService {
 
     private final RoleRepository roleRepository;
+    private final RoleMapper roleMapper;
 
-    public RoleServiceImpl(RoleRepository roleRepository) {
+    public RoleServiceImpl(RoleRepository roleRepository, RoleMapper roleMapper) {
         this.roleRepository = roleRepository;
+        this.roleMapper = roleMapper;
     }
 
     @Override
@@ -22,7 +27,10 @@ public class RoleServiceImpl implements RoleService {
         //but repository is providing entity (Role)
         //we need mapper
 
-        return null;
+        //convert entity to dto - Mapper
+        //get roles from db and convert each role to roleDto
+        return roleRepository.findAll().stream().map(roleMapper::convertToDto).collect(Collectors.toList());
+        //map(obj -> roleMapper.convertToDto(obj))
     }
 
     @Override
