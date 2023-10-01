393de54f48ae86ea4812e3c4d3fb9def5ec4e66a
irfan <23pointalpha@gmail.com>
2023-10-01 13:07:09 +0300

    User List table task (insert initial data) completed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,15 @@
 INSERT INTO roles(description) VALUES('Admin');
 INSERT INTO roles(description) VALUES('Manager');
 INSERT INTO roles(description) VALUES('Employee');
+
+-- Insert User 1
+INSERT INTO users (first_name, last_name, user_name, pass_word, enabled, phone, role_id, gender)
+VALUES ('John', 'Doe', 'john@example.com', 'Password123', true, '1234567890', 1, 'MALE');
+
+-- Insert User 2
+INSERT INTO users (first_name, last_name, user_name, pass_word, enabled, phone, role_id, gender)
+VALUES ('Jane', 'Smith', 'jane@example.com', 'Password456', true, '9876543210', 1, 'FEMALE');
+
+-- Insert User 3
+INSERT INTO users (first_name, last_name, user_name, pass_word, enabled, phone, role_id, gender)
+VALUES ('Bob', 'Johnson', 'bob@example.com', 'Password789', true, '4567891230', 1, 'MALE');
